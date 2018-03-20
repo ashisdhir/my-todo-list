ef7cb980b7cc28bf2d8cb4a00ff985685ee0debd
ashisdhir <ashis@example.com>
2018-03-20 14:52:05 +0530

    sign up page added.

--- worklookup/urls.py
+++ worklookup/urls.py
@@ -21,5 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todolist.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', auth_views.login, name='login'),
+    path('logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
+    # path('accounts/', include('django.contrib.auth.urls')),
 ]
