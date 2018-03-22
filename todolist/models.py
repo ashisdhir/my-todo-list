fd179efe742aaa6026309f92fc861828fa156b52
ashisdhir <ashis@example.com>
2018-03-22 15:23:34 +0530

    content added

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class todo(models.Model):
+    name = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    description = models.TextField()
+    created = models.DateTimeField()
+
+    def __str__(self):
+        return self.description
--- todolist/views.py
+++ todolist/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, forms
+from .models import todo
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def index(request):
     return render(request, 'todolist/index.html')
+
+@login_required()
+def details(request):
+    items = todo.objects.filter(name=request.user).order_by('created')[:10]
+
+    return render(request, 'todolist/details.html', {'items': items})
+
 
 def signup(request):
     if request.method == 'POST':
